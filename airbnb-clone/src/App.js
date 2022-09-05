2e62b374447a9362fb037bf58c49245be6e91b2b
Aman-Jamshed <amanjamshed3@gmail.com>
2022-09-05 23:15:01 +0530

    completed-airbnb-clone

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -2,23 +2,57 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import girl from "./images/girl.png";
+// import girl from "./images/girl.png";
+
+import cardData  from "./data"
+
 
 import "./style.css"
 
 export default function App() {
+    const cardElements = cardData.map(card => {
+        return ( <Card 
+            key={card.id}
+            // {...card}
+            card = {card}
+
+            // img={card.coverImg}
+            // rating={card.stats.rating}
+            // reviewCount={card.stats.reviewCount}
+            // country={card.location}
+            // title={card.title}
+            // price={card.price}
+            // openSpots={card.openSpots}
+            
+        />
+        )
+    })
+
+
+
+
+
+
     return(
         <div>
             <Navbar/>
             <Hero/>
-            <Card
+            <section className="cards-list">
+                {cardElements}
+            </section>
+            
+
+           
+            
+        </div>
+    )
+}
+
+/* <Card
              img={girl}
              rating="5.0"
              reviewCount={6}
              country="USA"
              title="Learn wedding photography"
              price={125}
-            />
-        </div>
-    )
-}
+            /> */
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -3,21 +3,64 @@ import star from "../images/star.png"
 // import girl from "../images/girl.png";
 
 export default function Card(props) {
+  console.log(props)
+  // let badgeText
+  // if(props.openSpots === 0) {
+  //   badgeText = "SOLD OUT"
+  // }else if(props.country === "Online") {
+  //   badgeText = "ONLINE"
+  // }
+
+
+  // return (  
+  // <div className="card">
+  //       {badgeText && <div className="card--badge">{badgeText}</div>}
+
+
+  //       <img src={props.img} alt="" className="card--image"/>
+
+  //       <div className="card--stats">
+  //            <img src={star} alt="" className="card--star"/> 
+  //            <span>{props.rating}</span>
+  //            <span className="gray">({props.reviewCount}). </span>
+  //            <span className="gray">{props.country}</span>
+  //       </div>
+  //       <p className="card--title">{props.title}</p>
+  //       <p className="card-price"><strong>From ${props.price}</strong> / person </p>
+
+  //   </div>
+
+
+  let badgeText
+  if(props.card.coverImg === 0) {
+    badgeText = "SOLD OUT"
+  }else if(props.card.location === "Online") {
+    badgeText = "ONLINE"
+  }
+
+
   return (  
   <div className="card">
-        {<img src={props.img} alt="" className="card--photo"/>}
+        {badgeText && <div className="card--badge">{badgeText}</div>}
+
+
+        <img src={props.card.coverImg} alt="" className="card--image"/>
 
         <div className="card--stats">
              <img src={star} alt="" className="card--star"/> 
-             <span>{props.rating}</span>
-             <span className="gray">{props.reviewCount} . </span>
-             <span className="gray">{props.country}</span>
+             <span>{props.card.stats.rating}</span>
+             <span className="gray">({props.card.stats.reviewCount}). </span>
+             <span className="gray">{props.card.location}</span>
         </div>
-        <p>{props.title}</p>
-        <p><strong>From ${props.price}</strong> / person </p>
+        <p className="card--title">{props.card.title}</p>
+        <p className="card--price"><strong>From ${props.card.price}</strong> / person </p>
 
     </div>
   )
     
 
 }
+
+
+
+        
